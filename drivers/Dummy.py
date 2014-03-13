62c74a56c185c481811fddb679e528c4a502582a
Dave Hocker <AtHomeX10@gmail.com>
2014-03-13 19:17:35 -0400

    Add logging to Dummy driver

--- drivers/Dummy.py
+++ drivers/Dummy.py
@@ -22,24 +22,23 @@ class Dummy(X10ControllerInterface.X10ControllerInterface):
   
   def __init__(self):
     X10ControllerInterface.X10ControllerInterface.__init__(self)
-    logger.info("X10 Dummy class initialized")
-    #print "House code for P:", Dummy.GetHouseCode("P")
-    #print "Device code for 16:", Dummy.GetDeviceCode(16)
+    logger.info("Dummy driver initialized")
     pass
     
   # Open the device
   def Open(self):
-    pass
+    logger.debug("Driver opened")
     
   # Close the device
   def Close(self):
-    pass    
+    logger.debug("Driver closed")
 
   #######################################################################
   # Turn a device on
   # house_device_code = Ex. 'A1'
   # dim_amount as a percent 0 <= v <= 100
   def DeviceOn(self, house_device_code, dim_amount):
+    logger.debug("DeviceOn for: %s %s", house_device_code, dim_amount)
     return True
 
   #######################################################################
@@ -47,6 +46,7 @@ class Dummy(X10ControllerInterface.X10ControllerInterface):
   # house_device_code = Ex. 'A1'
   # dim_amount 0 <= v <= 100
   def DeviceOff(self, house_device_code, dim_amount):
+    logger.debug("DeviceOff for: %s %s", house_device_code, dim_amount)
     return True
 
   #######################################################################
@@ -54,25 +54,29 @@ class Dummy(X10ControllerInterface.X10ControllerInterface):
   # house_device_code = Ex. 'A1'
   # dim_amount as a percent 0 <= v <= 100
   def DeviceDim(self, house_device_code, dim_amount):
+    logger.debug("DeviceDim for: %s %s", house_device_code, dim_amount)
     return True
 
   #######################################################################
   # Bright(en) a lamp module
   # house_device_code = Ex. 'A1'
   # bright_amount as a percent 0 <= v <= 100
-  def DeviceBright(self, house_device_code, dim_amount):
+  def DeviceBright(self, house_device_code, bright_amount):
+    logger.debug("DeviceBright for: %s %s", house_device_code, bright_amount)
     return True
 
   #######################################################################
   # Turn all units off (for a given house code)
   # house_code = "A"..."P"
   def DeviceAllUnitsOff(self, house_code):
+    logger.debug("DeviceAllUnitsOff for: %s", house_code)
     return True
 
   #######################################################################
   # Turn all lights off
   # house_code = "A"..."P"
   def DeviceAllLightsOff(self, house_code):
+    logger.debug("DeviceAllLightsOff for: %s", house_code)
     return True
 
   #######################################################################
@@ -86,7 +90,6 @@ class Dummy(X10ControllerInterface.X10ControllerInterface):
     pass
 
   #######################################################################
-  # TODO Consider defining this as SetCurrentTime taking no parameters.
   # Set the controller time to the current, local time.
   def SetTime(self, time_value):
     pass
@@ -97,12 +100,3 @@ class Dummy(X10ControllerInterface.X10ControllerInterface):
   # TODO Consider defining a method for each device function
   def ExecuteFunction(self, house_code, dim_amount, device_function):
     pass        
-    
-  # Return a datetime type
-  def GetTime(self):
-    pass        
-    
-  # TODO Consider defining this as SetCurrentTime taking no parameters.
-  # Set the controller time to the current, local time.
-  def SetTime(self, time_value):
-    pass            
