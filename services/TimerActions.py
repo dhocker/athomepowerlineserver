db3f4c2fb2f0a5750c29fcc16fcb211fb68d08ef
Dave Hocker <AtHomeX10@gmail.com>
2019-04-28 11:02:20 -0500

    Clean up code. Reformat to tab = 4 spaces.

--- services/TimerActions.py
+++ services/TimerActions.py
@@ -1,6 +1,6 @@
 #
 # AtHomePowerlineServer - networked server for CM11/CM11A/XTB-232 X10 controllers
-# Copyright (C) 2014  Dave Hocker
+# Copyright © 2014, 2019  Dave Hocker
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,33 +15,37 @@
 
 import drivers.X10ControllerAdapter
 
+
 ########################################################################
 # Action factory
 def GetAction(action_name):
-  ci_command = action_name.lower()
+    ci_command = action_name.lower()
 
-  if ci_command == "on":
-    action = On
-  elif ci_command == "off":
-    action = Off
-  elif ci_command == "dim":
-    action = Dim
-  else:
-    action = None
+    if ci_command == "on":
+        action = On
+    elif ci_command == "off":
+        action = Off
+    elif ci_command == "dim":
+        action = Dim
+    else:
+        action = None
 
-  return action
+    return action
+
 
 ########################################################################
 # Device/light on
 def On(house_device_code, dim_amount, args):
-  drivers.X10ControllerAdapter.X10ControllerAdapter.DeviceOn(house_device_code, dim_amount)
+    drivers.X10ControllerAdapter.X10ControllerAdapter.DeviceOn(house_device_code, dim_amount)
+
 
 ########################################################################
 # Device/light off
 def Off(house_device_code, dim_amount, args):
-  drivers.X10ControllerAdapter.X10ControllerAdapter.DeviceOff(house_device_code, dim_amount)
+    drivers.X10ControllerAdapter.X10ControllerAdapter.DeviceOff(house_device_code, dim_amount)
+
 
 ########################################################################
 # Lamp module dim
 def Dim(house_device_code, dim_amount, args):
-  drivers.X10ControllerAdapter.X10ControllerAdapter.DeviceDim(house_device_code, dim_amount)
+    drivers.X10ControllerAdapter.X10ControllerAdapter.DeviceDim(house_device_code, dim_amount)
